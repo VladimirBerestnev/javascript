4d6e6f8fa9c9958cd3b0bcb3b125018540a6d118
Vladimir Berestnev <berestnew@yandex.ru>
2023-05-29 21:48:56 +0300

    lesson10

---
+++ lesson10.js
@@ -0,0 +1,43 @@
+const data = ` [
+      {
+        "userId": 1,
+        "firstName": "Krish",
+        "lastName": "Lee",
+        "phoneNumber": "123456",
+        "emailAddress": "krish.lee@learningcontainer.com",
+        "photo": "photo1.jpg"
+      },
+      {
+        "userId": 2,
+        "firstName": "racks",
+        "lastName": "jacson",
+        "phoneNumber": "123456",
+        "emailAddress": "racks.jacson@learningcontainer.com",
+        "photo": "photo2.jpg"
+      },
+      {
+        "userId": 3,
+        "firstName": "denial",
+        "lastName": "roast",
+        "phoneNumber": "33333333",
+        "emailAddress": "denial.roast@learningcontainer.com",
+        "photo": "photo3.jpg"
+      },
+      {
+        "userId": 4,
+        "firstName": "devid",
+        "lastName": "neo",
+        "phoneNumber": "222222222",
+        "emailAddress": "devid.neo@learningcontainer.com",
+        "photo": "photo4.jpg"
+      },
+      {
+        "userId": 5,
+        "firstName": "jone",
+        "lastName": "mac",
+        "phoneNumber": "111111111",
+        "emailAddress": "jone.mac@learningcontainer.com",
+        "photo": "photo5.jpg"
+      }
+    ]
+  `
